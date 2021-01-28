a39827e29ed570bda3ffec841572853b89084a00
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2021-01-27 22:19:32 -0500

    get request written

---
+++ api/accounts/accounts-model.js
@@ -0,0 +1,31 @@
+const db = require("../../data/dbConfig");
+
+module.exports = {
+    get,
+    getById,
+    create,
+    update,
+    remove,
+}
+
+function get(){
+    return Promise.resolve('get wired');
+    db.('accounts')
+}
+
+function getById(){
+    return Promise.resolve('get wired');
+}
+
+function create(){
+    return Promise.resolve('get wired');
+}
+
+function update(){
+    return Promise.resolve('get wired');
+}
+
+function remove(){
+    return Promise.resolve('get wired');
+}
+
